8dcf63fec00a6c9d7e1571e9ffe23cc88ce8e56b
Tyree Christensen <tyree83@gmail.com>
2020-10-27 17:53:24 -0400

    updated js and css

--- js/script.js
+++ js/script.js
@@ -2,25 +2,28 @@ console.log("It's working!")
 
 function handleGetData(event) {
     event.preventDefault();
-  
+
     const searchText = $("#search").val()
     $.ajax({
-            url: `https://fortnite-api.com/v1/stats/br/v2/${searchText}?image=all` 
+            url: `https://fortnite-api.com/v1/stats/br/v2/${searchText}?image=all`
         })
         .then(
             (data) => {
+                userData = data.data.stats.all.overall;
                 console.log(data);
                 $("#wins").text(data.data.stats.all.overall.wins)
                 $("#top5").text(data.data.stats.all.overall.top5)
                 $("#top10").text(data.data.stats.all.overall.top10)
                 $("#kills").text(data.data.stats.all.overall.kills)
-                $("poster").attr("src", data.data.image)
+                $("#image").attr("src", data.data.image)
             },
             (error) => {
                 console.log("bad request: ", error)
             }
         )
 }
-$('form').on("submit", handleGetData)
 
+
+
+$('form').on("submit", handleGetData)
 
