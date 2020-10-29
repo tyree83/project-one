72df264739cf851f6528815afd476324404facb0
Tyree Christensen <tyree83@gmail.com>
2020-10-29 10:45:51 -0400

    added social links

--- js/script.js
+++ js/script.js
@@ -4,18 +4,15 @@ function handleGetData(event) {
     event.preventDefault();
 
     const searchText = $("#search").val()
+
     $.ajax({
             url: `https://fortnite-api.com/v1/stats/br/v2/${searchText}?image=all`
         })
         .then(
             (data) => {
-                userData = data.data.stats.all.overall;
+                userData = data.data;
                 console.log(data);
-                $("#wins").text(data.data.stats.all.overall.wins)
-                $("#top5").text(data.data.stats.all.overall.top5)
-                $("#top10").text(data.data.stats.all.overall.top10)
-                $("#kills").text(data.data.stats.all.overall.kills)
-                $("#image").attr("src", data.data.image)
+                $("#image").attr("src", userData.image)
             },
             (error) => {
                 console.log("bad request: ", error)
